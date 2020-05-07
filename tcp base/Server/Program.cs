e99978a7d22ed3fb4800827e4e6f68843aa8f194
Yang Xiang <yangxiangpro@gmail.com>
2020-05-07 18:22:12 +0200

    Add files via upload

---
+++ tcp base/Server/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace serversocket
+{
+    class Program
+    {
+        static byte[] Buffer { get; set; }
+        static Socket sck;
+        static void Main(string[] args)
+        {
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sck.Bind(new IPEndPoint(0, 1234));
+            sck.Listen(100);
+            Socket accepted = sck.Accept();
+            Buffer = new byte[accepted.SendBufferSize];
+            int bytesRead = accepted.Receive(Buffer);
+            byte[] formatted = new byte[bytesRead];
+            for (int i = 0; i < bytesRead; i++)
+            {
+                formatted[i] = Buffer[i];
+            }
+            Console.Write("\t Server" + "\n");
+            String stradata = Encoding.ASCII.GetString(formatted);
+            Console.Write("-->" + " " + stradata + "\n\n");
+            Console.Read();
+            sck.Close();
+            accepted.Close();
+
+        }
+    }
+}
+
+
+
+
---
+++ tcp base/tcp base/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ClientSocket
+{
+    class Program
+    {
+        static Socket skc;
+        static void Main(string[] args)
+        {
+            skc = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+            try
+            {
+                skc.Connect(localEndPoint);
+
+                Console.Write("\n Inserisci il testo:");
+                string text = Console.ReadLine();
+                byte[] data = Encoding.ASCII.GetBytes(text);
+                skc.Send(data);
+                Console.Write("\n Messaggio inviato correttamente");
+                Console.Write("\n Premi un tasto per continuare...");
+                Console.Read();
+                skc.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.Write("\n Connessione fallita:" + " " + ex.Message);
+                Main(args);
+            }
+
+        }
+    }
+}
